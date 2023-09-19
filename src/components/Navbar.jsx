cc2241b7a90985e1a34b32688007ecd285607fc5
Kevin Villegas <kevin.villegas1973@gmail.com>
2023-09-19 10:30:07 -0300

    fixing cv

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
 import { useState } from 'react';
-import CV from '../assets/documents/cv.pdf';
+import cv from '../assets/documents/cv.pdf';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -167,7 +167,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href={CV}
+              href={cv}
               target='_blank'
             >
               Resume <BsFillPersonLinesFill size={30} />
