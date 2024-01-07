0453e2911516948f069e9d93bf7ce4b1bf455e6b
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-01-07 14:16:02 -0300

    Adding MambaUI Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -22,6 +24,34 @@ export default {
       // Extra large screens or high resolution screens
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      rose: colors.rose,
+      pink: colors.pink,
+      fuchsia: colors.fuchsia,
+      purple: colors.purple,
+      violet: colors.violet,
+      indigo: colors.indigo,
+      blue: colors.blue,
+      sky: colors.sky, // As of Tailwind CSS v2.2, `lightBlue` has been renamed to `sky`
+      cyan: colors.cyan,
+      teal: colors.teal,
+      emerald: colors.emerald,
+      green: colors.green,
+      lime: colors.lime,
+      yellow: colors.yellow,
+      amber: colors.amber,
+      orange: colors.orange,
+      red: colors.red,
+      slate: colors.slate,
+      zinc: colors.zinc,
+      gray: colors.gray,
+      neutral: colors.slate,
+      stone: colors.stone,
     },
   },
   plugins: [],
