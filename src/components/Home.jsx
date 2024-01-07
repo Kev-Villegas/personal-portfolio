d3b0ec9e5e5c6068d48416e7ec0c4d6a7d73c2b4
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-01-07 12:36:44 -0300

    Changue The Home Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
-import { HiArrowNarrowRight } from 'react-icons/hi';
+import { GoBriefcase } from 'react-icons/go';
+import { CiMail } from 'react-icons/ci';
 import { Link } from 'react-scroll';
+import React from 'react';
 
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-pink-600'>Hi, My Name Is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+      <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center h-full'>
+        <p className='text-pink-600 text-2xl'>Hi, My Name Is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-gray-200'>
           Kevin Villegas
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+        <h2 className='text-4xl sm:text-7xl font-bold text-gray-300'>
           I'm a Front-End Developer.
         </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+        <p className='text-gray-400 py-4 text-xl max-w-[800px]'>
           Front-End Developer Specializing in building and ocassionally
           designing exepcional digital experiences. Currently, I'm focused on
           become a Full-Stack Developer!
         </p>
-        <Link to='work' smooth={true} duration={500}>
-          <button className='rounded-md text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
-            View Work
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3 ' />
-            </span>
-          </button>
-        </Link>
+        <div className='flex items-center gap-3 md:flex-row'>
+          {/* Primer botón */}
+          <Link to='contact' smooth={true} duration={500} className='mr-4'>
+            <button
+              type='button'
+              className='w-full md:w-auto rounded-md text-white group border-2 px-6 py-3 flex items-center hover:bg-pink-600 hover:border-pink-600'
+            >
+              <CiMail size={24} className='mr-2' />
+              Contact Me
+              <span className='group-hover:rotate-90 duration-300 ml-3'></span>
+            </button>
+          </Link>
+
+          {/* Segundo botón */}
+          <Link to='work' smooth={true} duration={500}>
+            <button className='w-full md:w-auto rounded-md text-white group border-2 px-6 py-3 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+              <GoBriefcase size={24} className='mr-2' />
+              View Work
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
