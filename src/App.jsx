81008d7b2e1385ce3d95a3c780457478717c9648
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-01-09 09:36:51 -0300

    Adding Services Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from './components/About';
 import Contact from './components/Contact';
 import Home from './components/Home';
+import Services from './components/Services';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
 import Work from './components/Work';
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Services />
       <Skills />
       <Work />
       <Contact />
