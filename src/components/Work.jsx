417807f8da2a9b4eb9f05ebfedebcdc9d855498b
Kev-Villegas <kevin.villegas1973@gmail.com>
2024-01-25 12:37:14 -0300

    New Work Component, I Still need to add the framer motion effect

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,127 +1,95 @@
-import gameHub from '../assets/projects/gameHub.webp';
 import Portfolio from '../assets/projects/portfolioCover.webp';
-import ToDoList from '../assets/projects/toDoList.webp';
+import gymDesign from '../assets/projects/gymDesign.webp';
+import gameHub from '../assets/projects/gameHub.webp';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
+import React from 'react';
 
 const Work = () => {
   return (
-    <div
-      name='work'
-      className='w-full h-[450px] px-6 py-6 pl-12 bg-[#0a192f] text-gray-300'
+    <section
+      className='w-full h-[1000px] md:h-[1600px] lg:h-[1000px] xs:h-[1800px] px-6 py-6 bg-[#0a192f]'
+      id='work'
     >
-      <motion.div
-        variants={fadeIn('left', 0.5)}
-        initial='hidden'
-        whileInView={'show'}
-        viewport={{ once: true, amount: 0.3 }}
-        className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full'
-      >
-        <div>
-          <p className='text-4xl font-bold inline border-b-[3px] border-pink-600 font-lato'>
-            Work
-          </p>
-          <p className='py-4 md:text-xl sm:text-2xl font-montserrat font-medium'>
-            Check My Projects Below
-          </p>
-        </div>
-        {/* Container */}
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4'>
-          {/* Card 1 */}
-          <div
-            style={{ backgroundImage: `url(${gameHub})` }}
-            className='shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Game Discovery App
-              </span>
-              <div className='pt-8 text-center'>
-                <a
-                  href='https://video-game-discovery-app-liard.vercel.app/'
-                  target='_blank'
-                >
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href='https://github.com/Kev-Villegas/videoGames-discovery-app'
-                  target='_blank'
-                >
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
+      <div className='max-w-[1000px] xs:w-full mx-auto px-4 flex flex-col justify-center w-full h-full'>
+        <div className='flex flex-col lg:flex-row gap-x-10 '>
+          <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+            {/* Text */}
+            <div>
+              <h2 className='text-4xl leading-tight text-accent font-primary'>
+                My Latest Work
+              </h2>
+              <p className='max-w-sm py-4 font-secondary'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+                hic nobis, vel deleniti amet pariatur voluptas voluptate
+                suscipit culpa nam.
+              </p>
+              <button className='btn btn-outline mb-[22px] font-tertiary w-[170px] '>
+                View All Projects
+              </button>
+            </div>
+
+            {/* Project 1 */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl gap-y-10'>
+              {/* Overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-15 transition-all duration-300'></div>
+              <img
+                className='group-hover:opacity-10 group-hover:scale-125 transition-all duration-500'
+                src={gameHub}
+                alt='gameHub'
+              />
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-15'>
+                <span className='text-4xl text-green-700'>
+                  Front-End Project
+                </span>
+              </div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-15'>
+                <span className='text-3xl text-white'>Rawg Game Hub</span>
               </div>
             </div>
           </div>
 
-          {/* Card 2 */}
-          <div
-            style={{ backgroundImage: `url(${Portfolio})` }}
-            className='shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                Portfolio
-              </span>
-              <div className='pt-8 text-center'>
-                <a
-                  href='https://personal-portfolio-alpha-henna.vercel.app/'
-                  target='_blank'
-                >
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href='https://github.com/Kev-Villegas/personal-portfolio'
-                  target='_blank'
-                >
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
+          {/* Project 2 & 3 */}
+          <div className='flex-1 flex flex-col gap-y-12'>
+            {/* Project 2 */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* Overlay */}
+              <div className='group-hover:bg-black/100 group-hover:bg-blend-darken w-full h-full absolute z-15 transition-all duration-300'></div>
+              <img
+                className='group-hover:opacity-30 group-hover:scale-125 transition-all duration-500'
+                src={Portfolio}
+                alt='Portfolio'
+              />
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-15'>
+                <span className='text-4xl text-green-700'>
+                  Front-End Project
+                </span>
+              </div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-15'>
+                <span className='text-3xl text-white'>Personal Portfolio</span>
               </div>
             </div>
-          </div>
-          {/* Card 3 */}
-          <div
-            style={{ backgroundImage: `url(${ToDoList})` }}
-            className='shadow-lg shadow-[#040C16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-2xl font-bold text-white tracking-wider'>
-                To-Do List
-              </span>
-              <div className='pt-8 text-center'>
-                <a
-                  href='https://to-do-list-rouge-phi.vercel.app/'
-                  target='_blank'
-                >
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href='https://github.com/Kev-Villegas/to-do-list'
-                  target='_blank'
-                >
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
+
+            {/* Project 3 */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* Overlay */}
+              <div className='group-hover:bg-black/100 group-hover:bg-blend-darken w-full h-full absolute z-15 transition-all duration-300'></div>
+              <img
+                className='group-hover:opacity-40 group-hover:scale-125 transition-all duration-500'
+                src={gymDesign}
+                alt='gymDesign'
+              />
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-15'>
+                <span className='text-4xl text-green-700'>UX/UI Design</span>
+              </div>
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-15'>
+                <span className='text-3xl text-white'>Gym Landing Page</span>
               </div>
             </div>
           </div>
         </div>
-      </motion.div>
-    </div>
+      </div>
+    </section>
   );
 };
-
 export default Work;
